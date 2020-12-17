bf974bc3d76344bdb012605543fcfe997eed3486
jamalchelski <ifista201522035@gmail.com>
2020-12-17 22:16:50 +0700

    ok show all data from db

--- controller.js
+++ controller.js
@@ -6,3 +6,16 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok('aplikasi REST API berjalan !', res);
 };
+
+
+// show all data users
+
+exports.showallusers = function (req, res) {
+    connection.query('SELECT * FROM tbl_users', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
