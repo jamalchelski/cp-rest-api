206ad53deebb552a77bc7647c6a872620c6404a1
jamalchelski <ifista201522035@gmail.com>
2020-12-17 21:56:59 +0700

    setting routes js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+// call rauters
+
+var routers = require('./routers');
+routers(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
